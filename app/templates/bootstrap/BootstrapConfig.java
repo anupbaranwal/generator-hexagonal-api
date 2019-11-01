d08fe834284e2c55c230bdd084cdfe7474d1e684
Anup Kumar <anup.raj1989@gmail.com>
2019-11-01 15:23:53 +0530

    feat(swagger): Added swagger configuration for rest adapter
    Closes https://github.com/anupbaranwal/generator-hexagonal-app/issues/3

--- app/templates/bootstrap/BootstrapConfig.java
+++ app/templates/bootstrap/BootstrapConfig.java
@@ -1,11 +1,18 @@
 package <%=group%>.config;
 
 import <%=group%>.infra.config.<% if(jpaSupport){%>JpaAdapterConfig<%} else {%>HardcodedAdapterConfig<%}%>;
-import <%=group%>.rest.config.RestAdapterConfig;
+import <%=group%>.domain.MusicReaderService;
+import <%=group%>.domain.port.MusicReader;
+import <%=group%>.domain.port.MusicRepository;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 
 @Configuration
-@Import({RestAdapterConfig.class,
-        <% if(jpaSupport){%>JpaAdapterConfig.class<%} else {%>HardcodedAdapterConfig.class<%}%>})
-public class BootstrapConfig {}
+@Import(<% if(jpaSupport){%>JpaAdapterConfig.class<%} else {%>HardcodedAdapterConfig.class<%}%>)
+public class BootstrapConfig {
+  @Bean
+  public MusicReader getMusicReader(MusicRepository musicRepository) {
+    return new MusicReaderService(musicRepository);
+  }
+}
--- app/templates/rest/MusicResource.java
+++ app/templates/rest/MusicResource.java
@@ -2,11 +2,11 @@ package <%=group%>.rest;
 
 import <%=group%>.domain.model.MusicDto;
 import <%=group%>.domain.port.MusicReader;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
 import java.util.List;
 
 @RestController
@@ -17,6 +17,7 @@ public class MusicResource {
     private MusicReader musicReader;
 
     @GetMapping
+    @ApiOperation(value = "Fetches the musics from library")
     public List<MusicDto> getMusics() {
         return musicReader.readMusic();
     }
